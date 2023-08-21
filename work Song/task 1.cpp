f250457c6c68357fdddadb9b20d7f909c423eaa9
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-21 23:29:14 +0300

    Add files via upload

---
+++ work Song/task 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>;
+using std::cout;
+using std::endl;
+int main() {
+	cout << "Singers of the song: Vaya Con Dios " << endl;
+	cout << "Song: Nah neh nah" << endl;
+	cout << " I got on the phone and called" << endl;
+	cout << "the girls, said" << endl;
+	cout << "Meet me down at Curly Pearls," << endl;
+	cout << " for a" << endl;
+	cout << "Ney, Nah Neh Nah" << endl;
+	cout << " In my high - heeled shoes and fancy fads" << endl;
+	cout << "I ran down the stairs hailed me" << endl;
+	cout << " a cab, going" << endl;
+	cout << " Ney, Nah Neh Nah" << endl;
+	cout << " Ney, Nah Neh Nah" << endl;
+	cout << "Ney, Nah Neh Nah" << endl;
+	cout << "Nah Neh Nah" << endl;
+}
---
+++ work Song/task 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+int main()
+{
+	cout << "Every" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\t\tWants" << "\n";
+	cout << "\t\t\tTo know" << "\n";
+	cout << "\t\t\t\t Where" << "\n";
+	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+
+}
