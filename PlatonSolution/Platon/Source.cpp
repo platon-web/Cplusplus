6557cad69f88de01c75faf101e45c9982bdb8c01
Platon Holovetskyi <66998856+platon-web@users.noreply.github.com>
2023-08-21 23:26:55 +0300

    first work
    first c++ task

---
+++ PlatonSolution/Platon/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+
+using namespace std;
+int main() {
+	int r;
+	int r1;
+	int r2;
+	int r0;
+	cout << "tipes a R1";
+	cin >> r;
+	cout << "tipes a R2";
+	cin >> r1;
+	cout << "tipes a R3";
+	cin >> r2;
+	r0 = 1/r + 1/r1 + 1/r2;
+	cout << "R0=";
+	
+}
